0c47fae7737e325fa0906582c9df8c1b8cc8bdc5
Niklas Haiden <niklas.haiden@gmail.com>
2024-06-16 19:46:11 +0200

    feat(download): add doc and introduction links for aurora

---
+++ components/sections/download/check-docs.tsx
@@ -0,0 +1,14 @@
+export default function CheckDocs() {
+  return (
+    <div className={"w-full"}>
+      <div className={"italic text-aurora-darkblue"}>
+        Checkout the <a className={"underline"} href={"https://universal-blue.discourse.group/docs?topic=2291"}>docs
+        here.</a>
+      </div>
+      <div className={"italic text-aurora-darkblue"}>
+        Checkout the <a className={"underline"} href={"https://universal-blue.discourse.group/docs?topic=1235"}>Introduction
+        to Aurora.</a>
+      </div>
+    </div>
+  );
+}
