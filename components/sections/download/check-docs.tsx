f544f350dc9f448231c38c6a917a2eb37f28abdb
Pat Connors <121328689+nicknamenamenick@users.noreply.github.com>
2024-10-11 14:29:45 -0400

    forgot the most important part

--- components/sections/download/check-docs.tsx
+++ components/sections/download/check-docs.tsx
@@ -2,7 +2,7 @@ export default function CheckDocs() {
   return (
     <div className={"w-full"}>
       <div className={"italic text-aurora-darkblue"}>
-        Check out the <a className={"underline"} href={"https://docs.projectbluefin.io/"}> documentation
+        Check out the <a className={"underline"} href={"https://docs.getaurora.dev/"}> documentation
         here.</a>
       </div>
       <div className={"italic text-aurora-darkblue"}>
