9be4ff8704d1c304108527e143366a3087ca7cf4
Pat Connors <121328689+nicknamenamenick@users.noreply.github.com>
2024-10-11 14:35:56 -0400

    Less clutter

--- components/sections/download/check-docs.tsx
+++ components/sections/download/check-docs.tsx
@@ -2,12 +2,8 @@ export default function CheckDocs() {
   return (
     <div className={"w-full"}>
       <div className={"italic text-aurora-darkblue"}>
-        Check out the <a className={"underline"} href={"https://docs.getaurora.dev/"}> documentation
+        Check out the <a className={"underline"} href={"https://docs.getaurora.dev/guides/intro/"}> documentation
         here.</a>
-      </div>
-      <div className={"italic text-aurora-darkblue"}>
-        Check out the <a className={"underline"} href={"https://docs.getaurora.dev/guides/intro/"}>Introduction
-        to Aurora.</a>
       </div>
     </div>
   );
