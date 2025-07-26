d05e774d430f9399c02a8ff6513c0142c2a49339
Niklas <40402114+NiHaiden@users.noreply.github.com>
2025-07-26 18:36:16 +0200

    Update docs.tsx

--- components/sections/about/docs.tsx
+++ components/sections/about/docs.tsx
@@ -121,7 +121,7 @@ export default function AboutDocs() {
                     Go to Forums <PersonStanding size={32} />
                   </a>
                   <a
-                    href={"https://discord.com/invite/jN2HHespdk"}
+                    href={"https://discord.gg/WEu6BdFEtp"}
                     target={"_blank"}
                     rel={"noreferrer"}
                     className={
