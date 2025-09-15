ecc60bab1066d240b270334b092206e9091e05e3
Niklas <40402114+NiHaiden@users.noreply.github.com>
2025-09-15 13:18:17 +0000

    fix: add missing closing tag

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -39,7 +39,7 @@ export default function Footer({ introRef }: { introRef: RefObject<any> }) {
           {t("proudly-built-phrase")}
         </div>
         <div>{t("built-with-love")}</div>
-        <div>Logos have been crafted by Delphic Melody (@furtivetardigrade on Discord) and @zandrro. ❤️
+        <div>Logos have been crafted by Delphic Melody (@furtivetardigrade on Discord) and @zandrro. ❤️</div>
         <div>Wallpapers & other art are made by <a className={"underline"} href={"https://ko-fi.com/chandeleer"} target={"_blank"}>Chandeleer</a> ❤️</div>
         <div
           className={
